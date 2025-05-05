20b2961c1afba0b5be15c42064d0a11433c9f4a5
Wagner Brüggemann <84648927+NamgRengaw@users.noreply.github.com>
2025-05-05 19:00:26 -0300

    [develop] Backend, frontend and database setup

---
+++ frontend/basilisk/src/views/CrudView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card bg-white text-black">
+        Card
+        <div class="flex">
+            <div>
+                Collumn A
+            </div>
+            <div>
+                Collumn B
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="css">
+
+.card {
+    border: 2px black solid;
+}
+
+.bg-white {
+    background-color: white;
+}
+
+.text-black {
+    color: black;
+}
+
+.flex {
+    display: flex;
+}
+
+</style>
